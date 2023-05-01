b6da97d0f3eb8ce1209d170ca95858091b7ec2f5
Khuong T.G. Hieu <ktgiahieu@gmail.com>
2023-05-01 18:44:59 +0200

    update new version

--- twitch_chat_analysis.py
+++ twitch_chat_analysis.py
@@ -1,123 +1,80 @@
 import sys
 import json
+import re
 from textblob import TextBlob
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.mllib.clustering import StreamingKMeans
+from collections import defaultdict
 
-# import spacy
+def clean_twitch_chat_message(message):
+    # Remove usernames
+    message = re.sub(r'@[\S]+', '', message)
+    # Remove emotes
+    message = re.sub(r':[a-z]+:', '', message)
+    # Remove URLs
+    message = re.sub(r'http\S+', '', message)
+    # Remove punctuation and digits
+    message = re.sub(r'[^\w\s]', '', message)
+    message = re.sub(r'\d+', '', message)
+    # Remove extra whitespace
+    message = re.sub(r'\s+', ' ', message)
+    # Remove leading/trailing whitespace
+    message = message.strip()
+    return message
+    
+def attach_sentiment(text): 
 
-# nlp = spacy.load("en_core_web_sm")
-topics = []
-def get_all_words(msg):
-    """Processes the messages to extract relevant keywords"""
-    # Extract keywords using Named Entity Recognition
-    doc = TextBlob(msg)
-    all_words = list(set([w for w in doc.words]))
-    return all_words
+    # create a TextBlob object for the text
+    blob = TextBlob(text)
 
-# def get_keywords(msg):
-#     """Processes the messages to extract relevant keywords"""
-#     # Extract keywords using Named Entity Recognition
-#     doc = nlp(msg)
-#     keywords = [ent.text for ent in doc.ents if ent.label_ == "ORG" or ent.label_ == "PERSON" or ent.label_ == "GPE"]
-#     return keywords
+    # get the sentiment polarity and subjectivity
+    polarity = blob.sentiment.polarity
 
-# # Define the function to perform sentiment analysis
-# def get_sentiment(message):
-#     text = message
-#     blob = TextBlob(text)
-#     sentiment = blob.sentiment.polarity
-#     return (sentiment, message)
+    return polarity
 
-# def sentiment_cluster(msg):
-#         try:
-#             sentiment = TextBlob(msg).sentiment.polarity
-#             if sentiment > 0.5:
-#                 return "Positive"
-#             elif sentiment < -0.5:
-#                 return "Negative"
-#             else:
-#                 return "Neutral"
-#         except:
-#             return "Error"
+def get_sentiments_each_game(message):
+    message =json.loads(message.strip(), strict=False)
 
-# # Define the function to get the location of the user
-# def get_location(message):
-#     user = message['user']
-#     if 'location' in user:
-#         location = user['location']
-#     else:
-#         location = 'Unknown'
-#     return (location, message)
+    sentiments = defaultdict(list)
+    for game_name, msg_string in message.items():
+        msgs = msg_string.split('\r\n:')
+        for msg in msgs:
+            if 'PRIVMSG' in msg:
+                msg = msg.split(':')[-1]
+                msg = clean_twitch_chat_message(msg)
+                sentiment = attach_sentiment(msg)
+                sentiments[game_name].append(sentiment)
+    return sentiments
 
-# Define the function to perform clustering
-def do_clustering(topics):
-    if len(topics) > 0:
-        # Convert the topics to a numerical vector using one-hot encoding
-        vocab = list(set(topics))
-        vector = [int(topic in topics) for topic in vocab]
-        
-        # Initialize the streaming k-means model
-        model = StreamingKMeans(k=3, decayFactor=0.5)
-        # Update the streaming k-means model with the vector
-        model.update([vector])
-        
-        # Get the current centroids of the model
-        centroids = model.latestModel().clusterCenters.tolist()
-        
-        # Emit the current centroids
-        yield centroids
+def combine_sentiments(sentiments_a, sentiments_b):
+    for game_name, sentiment_list in sentiments_b.items():
+        sentiments_a[game_name].extend(sentiment_list)
+    return sentiments_a
 
 # Define the function to extract data from the JSON string and perform analysis
 def process_stream(stream):
-    stream  = stream.window(60, 1)
-    messages = stream.filter(lambda x: "PRIVMSG" in x).map(lambda x: x.split(':')[-1])
-    all_words = messages.map(get_all_words).reduce(lambda a, b: a + b)
-    return all_words
-    # centroids = topics.flatMap(do_clustering)
-
-    # # location = sentiment.map(get_location)
-    # # labels = do_clustering(location)
-    # # window_counts = labels.countByValueAndWindow(60, 1)
-    # # print(window_counts)
-    # if len(topics) > 0:
-    #     # Convert the topics to a numerical vector using one-hot encoding
-    #     vocab = list(set(topics))
-    #     vector = [int(topic in topics) for topic in vocab]
-        
-    #     # Initialize the streaming k-means model
-    #     model = StreamingKMeans(k=3, decayFactor=0.5)
-    #     # Update the streaming k-means model with the vector
-    #     model.update([vector])
-        
-    #     # Get the current centroids of the model
-    #     centroids = model.latestModel().clusterCenters.tolist()
-        
-    #     # Emit the current centroids
-    #     yield centroids
-
+    stream = stream.window(60,1)
+    sentiments_each_game = stream.map(get_sentiments_each_game)
+    sentiments = sentiments_each_game.reduce(combine_sentiments)
+    
+    return sentiments
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: twitch_chat_analysis.py <hostname> <port>", file=sys.stderr)
         sys.exit(-1)
     sc = SparkContext(appName="TwitchChatAnalysis")
-    sc.setLogLevel("WARN")
+    sc.setLogLevel("ERROR")
     ssc = StreamingContext(sc, 1)
-    ssc.checkpoint("checkpoint")
-
-    # Define the parameters for the StreamingKMeans model
-    num_clusters = 5
-    decay_factor = 0.5
 
     stream = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    # stream.pprint()
-    centroids = process_stream(stream)
+    print('Content:')
+    stream.pprint()
 
-    
-    # Print the centroids to the console
-    centroids.pprint()
+    # Process the stream
+    sentiments = process_stream(stream)
+    sentiments.pprint()
 
     ssc.start()
     ssc.awaitTermination()
